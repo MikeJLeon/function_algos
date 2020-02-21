2c0865424287d32aef008e403ba68eb430679a75
Michael Cote <michaelcote7@outlook.com>
2020-02-20 16:12:45 -0800

    "killinit"

--- 1_sigma.js
+++ 1_sigma.js
@@ -1,9 +1,18 @@
 // given some number, print "adding up" to the console and add from 1 up to that numnber and return the sum
 // Predcted output: sigma(5) should return 15   (I.E: 1+2+3+4+5 =15)
 
+
+
+
 function sigma(num){
+   var counter = 0                    //1-
+for(var i = 1; i <= num; i++ ){
+    counter = counter + i;}
 
+return counter;
 
-    
 }
-    
+
+
+ result = sigma(5)   
+console.log(result)
--- 2_greater_than_y.js
+++ 2_greater_than_y.js
@@ -1,10 +1,22 @@
 // Given an array and a number y, count and return how many numbers in the array are bigger than y.
 // Predcted output: greaterThanY([1,6,8,3,5,2], 4) should return 3
 
+
+
 function greaterThanY(arr, y){
-    // YOUR CODE HERE
+
+    var counter = 0;
+
+     for (var i = 0; i < arr.length; i++) {   // i = 0 - 1 - 2 -
+              if(arr[i] > y){
+            counter  =  counter + 1;  }       //  counter = 0 - 0 - 1 - 2
 }
-var result = greaterThanY([1,6,8,3,5,2], 4);
-console.log(result);
+             return counter;
+
+
+}
+var output = greaterThanY([1,6,8,3,5,2], 4);
+console.log(output);
+
 
 
